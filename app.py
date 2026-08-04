5df68c096cfcac28f29f79499b5b0eaf5881d58b
fx-a11y <cendold820@gmail.com>
2026-08-04 21:15:49 +0700

    Add Twelve Data API integration

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = "8609166528:AAHDRNf1QQlwb5LWbU3L53apUEZV1buMWLY"
-CHAT_ID = "1723200337"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+API_KEY = os.getenv("TWELVE_DATA_API_KEY")
+
+def get_forex_price(symbol="XAU/USD"):
+    url = f"https://api.twelvedata.com/price?symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(url)
+    return response.json()
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    text = str(data)
+
+    price = get_forex_price("XAU/USD")
+
+    text = f"""
+Alert:
+{data}
+
+Harga XAUUSD:
+{price}
+"""
 
     requests.post(
-        f"https://api.telegram.org/bot{8609166528:AAHDRNf1QQlwb5LWbU3L53apUEZV1buMWLY}/sendMessage",
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         json={
-            "chat_id": 1723200337,
+            "chat_id": CHAT_ID,
             "text": text
         }
     )
@@ -23,3 +40,4 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
